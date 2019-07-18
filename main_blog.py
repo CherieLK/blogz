8bc2567b4cde8948785dacf18633a55711a2054a
Cherié Knapp <cherieseducation@outlook.com>
2019-07-17 20:29:00 -0500

    signup and login templates

--- main_blog.py
+++ main_blog.py
@@ -1,9 +1,9 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:mypassword@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:123@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
@@ -12,10 +12,23 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(300))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __init__(self, title, body):
         self.title = title
         self.body = body
+        self.owner = owner
+
+class User(db.Model): 
+
+    id = db.Column(db.Integer, primary_key=True) 
+    username = db.Column(db.String(120), unique=True)  
+    password = db.Column(db.String(120))
+    tasks = db.relationship('Blog', backref='owner')
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
 @app.route('/', methods=['GET'])
 def index():
@@ -37,6 +50,9 @@ def add_blog():
         blog_message = request.form['blog-message']
         if (not blog_message) or ( blog_message.strip() == ""):
             blogbody_error = "Please enter a blog message. "
+
+        logged_in_username = session['user'] 	
+        owner = User.query.filter_by(username=logged_in_username).first()
 
         if blogtitle_error != "":
             return render_template('new_post.html', title="Build A Blog", blogtitle_error=blogtitle_error, blog_message = blog_message)   
@@ -63,6 +79,18 @@ def get_ind_blog():
     blog_id = request.args.get('id')
     ind_blog = Blog.query.get(blog_id)
     return render_template('ind_blog.html', title="Individual Blog", ind_blog=ind_blog)
+
+@app.route('/login', methods=['GET', 'POST'])
+def get_username():
+
+    
+    return render_template('login.html', title="Login to Blogz")
+
+@app.route('/signup', methods=['GET', 'POST'])
+def get_new_username():
+
+     return render_template('signup.html', title="Signup for Blogz")
+
 
 
 if __name__ == '__main__':
